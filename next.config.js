6152a564a99aba741bd9391c458bdf7130616e6d
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-05 16:30:44 +0100

    Remove brackets from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   distDir: 'build',
-  assetPrefix: isProduction ? '/{c-hive.dev}' : '',
+  assetPrefix: isProduction ? '/c-hive.dev' : '',
 };
