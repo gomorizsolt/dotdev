ca78b42f3a57962876f241b2e697da4e52e2b851
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-05 23:25:03 +0100

    Add export script, update .gitignore and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
   distDir: 'build',
-  assetPrefix: isProduction ? 'https://c-hive.gitlab.io/c-hive.dev/' : '',
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+    };
+  },
 };
