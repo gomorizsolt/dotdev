ca22249d5901756eb0ef01a4ab5eddf2da1f8629
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-05 16:48:38 +0100

    Update assetPrefix path in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   distDir: 'build',
-  assetPrefix: isProduction ? '/c-hive.dev' : '',
+  assetPrefix: isProduction ? 'https://c-hive.gitlab.io/c-hive.dev/' : '',
 };
