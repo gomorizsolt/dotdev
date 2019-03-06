c8f2f32e8f2e821fd04943ecc203869b3491f531
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-06 22:17:50 +0100

    Remove next-source-maps

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
-// https://github.com/zeit/next-plugins/issues/309#issuecomment-432004655
-/* const withSourceMaps = require('@zeit/next-source-maps')();
-
-module.exports = withSourceMaps({
-  distDir: 'build',
-  exportPathMap() {
-    return {
-      '/': { page: '/' },
-    };
-  },
-  webpack(config) {
-    return config;
-  },
-}); */
+// @zeit/next-source-maps: https://github.com/zeit/next-plugins/issues/309#issuecomment-432004655
 
 module.exports = {
   distDir: 'build',
