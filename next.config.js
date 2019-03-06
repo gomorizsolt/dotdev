b40e7d505d40c2ce36f82e575ddc3c1f16181e08
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-06 21:59:41 +0100

    Add next-source-maps to get rid of 404 errors in console

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+const withSourceMaps = require('@zeit/next-source-maps')();
+
+module.exports = withSourceMaps({
   distDir: 'build',
   exportPathMap() {
     return {
       '/': { page: '/' },
     };
   },
-};
+  webpack(config) {
+    return config;
+  },
+});
