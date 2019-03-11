bb00161659bd9ef719bc57e450fcfb3cd6f8fabb
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-12 00:48:38 +0100

    Remove inadvertently installed packages and add comment to Users.js

--- resources/Users/Users.js
+++ resources/Users/Users.js
@@ -1,2 +1,4 @@
+// Might be a good idea to read from environment variables, but not necessary.
+
 export const GithubUsernames = ['gomorizsolt', 'thisismydesign', 'bencefrelli'];
 export const GitlabUsernames = ['gomori.zsolt1995', 'csaba.apagyi', 'bencevoros'];
