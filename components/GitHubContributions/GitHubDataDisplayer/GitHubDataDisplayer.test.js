c774dbb0811cecc5a5eabdc6bcff6aef77fb1aee
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-13 16:10:11 +0100

    Extend TestUtils.js with args and refine GitHubDataDisplayer.test.js

--- components/GitHubContributions/GitHubDataDisplayer/GitHubDataDisplayer.test.js
+++ components/GitHubContributions/GitHubDataDisplayer/GitHubDataDisplayer.test.js
@@ -4,7 +4,9 @@ import GitHubDataDisplayer from './GitHubDataDisplayer';
 import * as ContributionsDataUtils from '../../../utils/ContributionsDataUtils/ContributionsDataUtils';
 import { ContributionsContainer } from './GitHubDataDisplayer.style';
 
-jest.mock('../../../utils/ContributionsDataUtils/ContributionsDataUtils');
+jest.mock('../../../utils/ContributionsDataUtils/ContributionsDataUtils', () => require
+  .requireActual('../../../utils/TestUtils/TestUtils')
+  .mockOriginalFunctionality('../ContributionsDataUtils/ContributionsDataUtils'));
 
 describe('<GitHubDataDisplayer />', () => {
   let githubDataDisplayerWrapper;
@@ -29,6 +31,9 @@ describe('<GitHubDataDisplayer />', () => {
     expect(githubDataDisplayerWrapper.find(ContributionsContainer)).toHaveLength(1);
   });
 
+  // Without resetting the mock above,
+  // it'd fail because the function is called 6 times instead of 3.
+  // Reason: it doesn't automatically reset the value between the renders.
   it('calls ContributionsDataUtils.SumValuesByProp three times', () => {
     expect(ContributionsDataUtils.SumValuesByProp).toHaveBeenCalledTimes(3);
   });
