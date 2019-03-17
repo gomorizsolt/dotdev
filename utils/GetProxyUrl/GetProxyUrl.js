230942c1b94e26d2abeddf8c3565fb593c1fc0ce
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-17 21:18:13 +0100

    Re-add cors proxy

--- utils/GetProxyUrl/GetProxyUrl.js
+++ utils/GetProxyUrl/GetProxyUrl.js
@@ -1,3 +1,3 @@
-const getProxyUrl = userName => `https://github.com/users/${userName}/contributions`;
+const getProxyUrl = userName => `https://c-hive-proxy.herokuapp.com/https://github.com/users/${userName}/contributions`;
 
 export default getProxyUrl;
