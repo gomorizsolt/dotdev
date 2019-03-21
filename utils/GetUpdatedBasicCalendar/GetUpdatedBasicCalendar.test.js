0cb4fceca312b8e8b0652d3a13d6f56eb0f167c5
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-21 09:08:58 +0100

    Change the order of variables in GetUpdatedBasicCalendar

--- utils/GetUpdatedBasicCalendar/GetUpdatedBasicCalendar.test.js
+++ utils/GetUpdatedBasicCalendar/GetUpdatedBasicCalendar.test.js
@@ -2,8 +2,8 @@ import GetUpdatedBasicCalendar from './GetUpdatedBasicCalendar';
 
 describe('GetUpdatedBasicCalendar', () => {
   it('sets the first date to the current date substacted by one year', () => {
-    const expectedDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toLocaleDateString().replace(/. /g, '-').slice(0, 10);
     const updatedBasicCalendar = GetUpdatedBasicCalendar();
+    const expectedDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toLocaleDateString().replace(/. /g, '-').slice(0, 10);
 
     const actualDate = updatedBasicCalendar.children[0].children[0].children[0].attributes['data-date'];
 
