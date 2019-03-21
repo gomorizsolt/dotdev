7f50bc54511fa472fa17ecea13af9eca2a9a8d00
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-03-21 10:17:59 +0100

    Add SvgContainer, display calendar

--- components/App/App.js
+++ components/App/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { Header } from './App.style';
+import React, { Component, Fragment } from 'react';
+import { stringify } from 'svgson';
+import { Header, SvgContainer } from './App.style';
+import GetUpToDateCalendar from '../../utils/GetUpToDateCalendar/GetUpToDateCalendar';
 
-const app = () => (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.container = null;
+  }
+
+  componentDidMount() {
+    const svg = stringify(GetUpToDateCalendar());
+
+    this.container.innerHTML = svg;
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header>
+          <img src="static/logo/logo.png" alt="C-Hive" />
+          <p>C-Hive</p>
+        </Header>
+        <p>1,058 contributions in the last year</p>
+        <SvgContainer
+          ref={(el) => {
+            this.container = el;
+          }}
+        />
+      </Fragment>
+
+    );
+  }
+}
+
+/* const app = () => (
   <Header>
     <img src="static/logo/logo.png" alt="C-Hive" />
     <p>C-Hive</p>
   </Header>
-);
+); */
 
-export default app;
+export default App;
