06b3f6adbc9c42bc342a00325e973dde85ff4e46
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-06 10:36:51 +0200

    Replace sets with returns in Common.test.js

--- utils/CalendarUtils/Common/Common.test.js
+++ utils/CalendarUtils/Common/Common.test.js
@@ -6,7 +6,7 @@ describe('Common', () => {
     describe('when the total of the daily contributions is 0', () => {
       const totalDailyContributions = 0;
 
-      it('sets the default color', () => {
+      it('returns the default color', () => {
         const expectedFillColor = '#ebedf0';
 
         const actualFillColor = Common.getFillColor(totalDailyContributions);
@@ -18,7 +18,7 @@ describe('Common', () => {
     describe('when the total of the daily contributions is higher than 0 and less than 10', () => {
       const totalDailyContributions = 9;
 
-      it('sets the `#c6e48b` color', () => {
+      it('returns the `#c6e48b` color', () => {
         const expectedFillColor = '#c6e48b';
 
         const actualFillColor = Common.getFillColor(totalDailyContributions);
@@ -30,7 +30,7 @@ describe('Common', () => {
     describe('when the total of the daily contributions is higher than or equal to 10 and less than 20', () => {
       const totalDailyContributions = 19;
 
-      it('sets the `#7bc96f` color', () => {
+      it('returns the `#7bc96f` color', () => {
         const expectedFillColor = '#7bc96f';
 
         const actualFillColor = Common.getFillColor(totalDailyContributions);
@@ -42,7 +42,7 @@ describe('Common', () => {
     describe('when the total of the daily contributions is higher than or equal to 20 and less than 30', () => {
       const totalDailyContributions = 29;
 
-      it('sets the `#239a3b` color', () => {
+      it('returns the `#239a3b` color', () => {
         const expectedFillColor = '#239a3b';
 
         const actualFillColor = Common.getFillColor(totalDailyContributions);
@@ -54,7 +54,7 @@ describe('Common', () => {
     describe('when the total of the daily contributions is higher than or equal to 30', () => {
       const totalDailyContributions = 30;
 
-      it('sets the `#196127` color', () => {
+      it('returns the `#196127` color', () => {
         const expectedFillColor = '#196127';
 
         const actualFillColor = Common.getFillColor(totalDailyContributions);
