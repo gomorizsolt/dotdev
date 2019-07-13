46e59ba461697d262bfb6efa3f4290d3fb6fd3ea
nkapolcs <nagy.kapolcs@gmail.com>
2019-07-13 13:05:42 +0200

    loader component to UI related to #23

--- src/components/GlobalTheme/GlobalTheme.js
+++ src/components/GlobalTheme/GlobalTheme.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GetTheme from "../../utils/GetTheme/GetTheme";
+import Loader from "../UI/Loader/Loader";
 import * as CustomHooks from "../../utils/CustomHooks/CustomHooks";
 import { ThemeContext } from "../../utils/useTheme/useTheme";
 
@@ -8,39 +9,7 @@ const globalTheme = ({ children }) => {
   const [themeState, setThemeState] = CustomHooks.useDarkMode();
 
   if (!themeState.hasThemeLoaded) {
-    return (
-      <div className="loader">
-        <style jsx>
-          {`
-            .loader {
-              display: block;
-              width: 58px;
-              height: 58px;
-              margin: 50vh auto;
-            }
-            .loader:after {
-              content: " ";
-              display: block;
-              width: 46px;
-              height: 46px;
-              margin: 1px;
-              border-radius: 50%;
-              border: 5px solid #325359;
-              border-color: #325359 transparent #325359 transparent;
-              animation: loader 1.2s linear infinite;
-            }
-            @keyframes loader {
-              0% {
-                transform: rotate(0deg);
-              }
-              100% {
-                transform: rotate(360deg);
-              }
-            }
-          `}
-        </style>
-      </div>
-    );
+    return <Loader />;
   }
 
   const toggle = () => {
