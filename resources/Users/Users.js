a8a7b02d0f661fe60dc31c5b09c61c05c1479d04
nkapolcs <nagy.kapolcs@gmail.com>
2019-07-13 14:36:32 +0200

    update gitlab users related to #23

--- resources/Users/Users.js
+++ resources/Users/Users.js
@@ -1,2 +1,2 @@
 export const githubUsernames = ["gomorizsolt", "thisismydesign", "megant", "nkapolcs"];
-export const gitlabUsernames = ["gomori.zsolt1995", "csaba.apagyi", "bencevoros", "nkapolcs"];
+export const gitlabUsernames = ["gomori.zsolt1995", "csaba.apagyi", "megant", "nkapolcs"];
