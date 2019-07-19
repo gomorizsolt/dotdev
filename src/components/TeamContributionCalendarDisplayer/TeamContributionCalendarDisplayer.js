a1d3daa1aaf95efd35679919e0b13df3d116b92f
nkapolcs <nagy.kapolcs@gmail.com>
2019-07-19 12:38:01 +0200

    resource folder to src related to #23

--- src/components/TeamContributionCalendarDisplayer/TeamContributionCalendarDisplayer.js
+++ src/components/TeamContributionCalendarDisplayer/TeamContributionCalendarDisplayer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import TeamContributionCalendar from "@c-hive/team-contribution-calendar";
-import * as Users from "../../../resources/Users/Users";
+import * as Users from "../../users/Users";
 
 class TeamContributionCalendarDisplayer extends Component {
   constructor(props) {
