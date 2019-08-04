959a7c203652cfd480d5de69b467e93c5842a641
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-08-04 12:46:50 +0200

    Add react/sort-comp rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/prop-types": 0,
+    "react/sort-comp": "off", // https://github.com/yannickcr/eslint-plugin-react/issues/1214
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
     "import/prefer-default-export": 0, // https://stackoverflow.com/q/54245654/2771889
     "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"], // // https://stackoverflow.com/q/39114446/2771889
