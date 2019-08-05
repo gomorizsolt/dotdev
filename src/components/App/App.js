334511db02096c96652b6acd317a0c271ea6ff21
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-08-05 16:07:44 +0200

    Remove containers folder

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import TeamContributionCalendarDisplayer from "../TeamContributionCalendarDisplayer/TeamContributionCalendarDisplayer";
+import Header from "../Header/Header";
+import Medium from "../Medium/Medium";
+import OneCol from "../UI/Layout/OneCol";
+import TwoCol from "../UI/Layout/TwoCol";
+import { appStyle } from "./App.style";
+
+const App = styled.div`
+  ${appStyle}
+`;
+
+const app = () => (
+  <Fragment>
+    <Header />
+    <App>
+      <OneCol>
+        <TeamContributionCalendarDisplayer />
+      </OneCol>
+      <TwoCol>
+        <Medium />
+      </TwoCol>
+    </App>
+  </Fragment>
+);
+
+export default app;
