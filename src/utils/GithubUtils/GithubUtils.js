78491e7d4d12751017ccfcc6d457573833683aa9
nkapolcs <nagy.kapolcs@gmail.com>
2019-09-14 23:26:33 +0200

    github project details related to #20

--- src/utils/GithubUtils/GithubUtils.js
+++ src/utils/GithubUtils/GithubUtils.js
@@ -1,13 +1,3 @@
-export const getUserGitHubProjectDetails = (userName, repositoryName) => {
-  const url = `https://api.github.com/repos/${userName}/${repositoryName}`;
-
-  return fetch(url, {
-    headers: {
-      Accept: "application/vnd.github.mercy-preview+json",
-    },
-  }).then(fetchedProjectDetails => fetchedProjectDetails.json());
-};
-
 export const getAllOrgGitHubRepos = userName => {
   const url = `https://api.github.com/orgs/${userName}/repos`;
 
