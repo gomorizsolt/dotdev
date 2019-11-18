67ed3ad0b0b5a49b9eef28d4f05e2aacd355ca92
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-18 14:07:36 +0100

    Add 'deploy' script

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
       "/": { page: "/" },
     };
   },
-  assetPrefix: "./",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/c-hive.dev" : "", // https://github.com/zeit/next.js/wiki/Deploying-a-Next.js-app-into-GitHub-Pages#extra-step-for-project-page-sites
 };
