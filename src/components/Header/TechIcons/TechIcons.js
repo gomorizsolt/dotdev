26ba262faa478df5059b6f178383eacd9815f447
nkapolcs <nagy.kapolcs@gmail.com>
2020-01-19 00:15:36 +0100

    create TeamMembers component

--- src/components/Header/TechIcons/TechIcons.js
+++ src/components/Header/TechIcons/TechIcons.js
@@ -18,9 +18,10 @@ const techIcons = () => (
           backgroundColor={settings.technologyIcons[name][1]}
         />
       ) : (
+        /* eslint-disable-next-line no-console */
         console.error(
           `Warning: There is no icon path specified in the settings for ${name} techology`
-        ) /* eslint-disable-line no-console */
+        )
       )
     )}
   </TechIcons>
