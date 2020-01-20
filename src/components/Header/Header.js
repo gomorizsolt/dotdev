b6f89a3fc495e1cf15d908efe524570b8a652cb8
nkapolcs <nagy.kapolcs@gmail.com>
2020-01-20 23:22:14 +0100

    rename iconOrName settings to display

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ const header = () => (
     <ToggleButton />
     {settings.header && settings.header.technologies ? (
       <div className="icons__container">
-        {settings.header.iconOrName === "icon" ? <TechIcons /> : <TechNames />}
+        {settings.header.display === "icon" ? <TechIcons /> : <TechNames />}
         {settings.header.teamMembers ? <TeamMembers /> : null}
       </div>
     ) : null}
