d700846f96eca261ce1a3c321e1d8bfc715306df
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-01-30 22:57:52 +0100

    Extract separate classes into their own style definitions in header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import settings from "../../../settings/settings.json";
-import { headerStyle } from "./Header.style";
+import {
+  headerStyle,
+  headerIconsContainerStyle,
+  logoTextStyle,
+  logoStyle,
+} from "./Header.style";
 import ToggleButton from "./ToggleButton/ToggleButton";
 import TechIcons from "./TechIcons/TechIcons";
 import TechNames from "./TechNames/TechNames";
@@ -11,18 +16,28 @@ const Header = styled.div`
   ${headerStyle}
 `;
 
+const HeaderIconsContainer = styled.div`
+  ${headerIconsContainerStyle}
+`;
+
+const LogoText = styled.p`
+  ${logoTextStyle}
+`;
+
+const Logo = styled.img`
+  ${logoStyle}
+`;
+
 const header = () => (
   <Header>
-    {settings.logo ? (
-      <img className="Logo" src={settings.logo} alt={settings.name} />
-    ) : null}
-    {settings.name ? <p className="Logo__text">{settings.name}</p> : null}
+    {settings.logo ? <Logo src={settings.logo} alt={settings.name} /> : null}
+    {settings.name ? <LogoText>{settings.name}</LogoText> : null}
     <ToggleButton />
     {settings.header && settings.header.technologies ? (
-      <div className="icons__container">
+      <HeaderIconsContainer>
         {settings.display === "icon" ? <TechIcons /> : <TechNames />}
         {settings.header.teamMembers ? <TeamMembers /> : null}
-      </div>
+      </HeaderIconsContainer>
     ) : null}
   </Header>
 );
