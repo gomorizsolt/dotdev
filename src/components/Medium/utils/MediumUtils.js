42e8179595564003297201e8687f1f11e6cf84dd
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-04-21 11:27:32 +0200

    Use 'url-parse' to proxify URLs

--- src/components/Medium/utils/MediumUtils.js
+++ src/components/Medium/utils/MediumUtils.js
@@ -1,10 +1,19 @@
 import RSSParser from "rss-parser";
+import Url from "url-parse";
 
 const filterItems = items => items.filter(item => item.categories);
 
+const proxify = (proxy, url) => {
+  const proxifiedUrl = new Url(proxy);
+
+  proxifiedUrl.set("pathname", url);
+
+  return proxifiedUrl;
+};
+
 export const getArticles = async (proxy, username) => {
   const rssParser = new RSSParser();
-  const feedUrl = `${proxy}https://medium.com/feed/${username}`;
+  const feedUrl = proxify(proxy, `https://medium.com/feed/${username}`);
 
   return rssParser
     .parseURL(feedUrl)
