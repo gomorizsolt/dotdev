7ca96d3a67d043b53b5c632ae9af8676eb5c35d1
gomorizsolt <30864202+gomorizsolt@users.noreply.github.com>
2020-05-11 07:14:25 +0000

    Deploying to gh-pages from  @ 2de252bef7e69800218bc30130ff8e689adef296 🚀

---
+++ _next/static/leDdUvfIvxOFvHPX6cYlC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F451823fb.4cfbc91cf173726a200d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
