87f11bb36dc5e39757c25bf6116049de97772e79
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-05-12 09:24:57 +0200

    Improve custom `App` component.
    Since the site doesn't have blocking data requirements for every single page there's no need for the `getInitialProps()` invokation. This also enabled the 'Automatic Static Optimization' feature.r

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,17 @@
-import App from "next/app";
 import Head from "next/head";
 import React from "react";
-import config from "../config/config.example.yml";
+// The CI would complain about the missing config file if the rule was not ignored.
+// Reason: at build-time, the config comes from an environment variable so it's not committed directly to the repository.
+// eslint-disable-next-line import/no-unresolved
+import config from "../config/config.yml";
 
-export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+const app = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>{config.name}</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Head>
-          <title>{config.name}</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
-}
+export default app;
